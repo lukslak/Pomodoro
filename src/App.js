8c742a9df9089b299b0fefe8d6169be9bbdd66c3
Luke Cieslak <cieslak.luke@gmail.com>
2017-12-13 23:43:08 +0100

    Make buttons do something useful

--- src/App.js
+++ src/App.js
@@ -4,28 +4,30 @@ import './App.css';
 import Pomodoro from './components/Pomodoro/Pomodoro'
 import Button from './components/Button/Button'
 
+const initialState = {
+  pomodoros: [
+    {
+      status: 'ready'
+    },
+    {
+      status: 'ready'
+    },
+    {
+      status: 'ready'
+    },
+    {
+      status: 'ready'
+    }
+  ]
+}
+
 class App extends Component {
 
   constructor(props) {
     super(props)
-    this.state = {
-      pomodoros: [
-        {
-          status: 'ready'
-        },
-        {
-          status: 'ready'
-        },
-        {
-          status: 'ready'
-        },
-        {
-          status: 'ready'
-        }
-      ]
-    }
+    this.state = initialState
   }
-  
+
   updatePomodoro = (id, status) => {
     console.log(status);
     const updatedPomodoros = this.state.pomodoros.map((pomodoro, index) =>
@@ -34,17 +36,68 @@ class App extends Component {
         status
       }
     )
-    this.setState({pomodoros: updatedPomodoros});
+    this.setState({ pomodoros: updatedPomodoros });
   }
 
   renderPomodoros = () => {
-    return this.state.pomodoros.map((pomodoro, index) => 
+    return this.state.pomodoros.map((pomodoro, index) =>
       <Pomodoro key={index} id={index} status={pomodoro.status} onClick={this.updatePomodoro} />
     )
   }
 
   handleClick = (type) => {
     console.log(type + ' clicked');
+  }
+
+  resetPomodoros = () => {
+    this.setState({ pomodoros: initialState.pomodoros })
+  }
+
+  startPomodoro = () => {
+    const { pomodoros } = this.state
+    const isAnyReady = pomodoros.some(pomodoro => pomodoro.status === 'ready')
+    const isAnyRunning = pomodoros.some(pomodoro => pomodoro.status === 'running')
+    if (isAnyRunning || !isAnyReady) {
+      console.log('cannot start pomodoro')
+      return
+    }
+
+    let found = false
+    const updatedPomodoros = pomodoros.map(pomodoro => {
+      if (found || pomodoro.status !== 'ready') {
+        return pomodoro
+      } else {
+        found = true
+        return {
+          ...pomodoro,
+          status: 'running'
+        }
+      }
+    })
+    this.setState({ pomodoros: updatedPomodoros });
+  }
+
+  stopPomodoro = () => {
+    const { pomodoros } = this.state
+    const isAnyRunning = pomodoros.some(pomodoro => pomodoro.status === 'running')
+    if (!isAnyRunning) {
+      console.log('cannot stop pomodoro')
+      return
+    }
+
+    let found = false
+    const updatedPomodoros = pomodoros.map(pomodoro => {
+      if (found || pomodoro.status !== 'running') {
+        return pomodoro
+      } else {
+        found = true
+        return {
+          ...pomodoro,
+          status: 'completed' // or should it be ready?
+        }
+      }
+    })
+    this.setState({ pomodoros: updatedPomodoros });
   }
 
   render() {
@@ -58,9 +111,9 @@ class App extends Component {
           {this.renderPomodoros()}
         </div>
         <div className="buttons">
-          <Button label="start" type="start" onClick={this.handleClick} />
-          <Button label="stop" type="stop" onClick={this.handleClick} />
-          <Button label="reset" />
+          <Button label="start" type="start" onClick={this.startPomodoro} />
+          <Button label="stop" type="stop" onClick={this.stopPomodoro} />
+          <Button label="reset" onClick={this.resetPomodoros} />
         </div>
       </div>
     );
