697c6079388fc2fdab8e280e5c70ef5e375a20ca
Luke Cieslak <cieslak.luke@gmail.com>
2017-12-16 04:51:28 +0100

    Add redux dependency and hook redux to application

---
+++ src/store/pomodoro.js
@@ -0,0 +1,23 @@
+const initialState = [
+    {
+        status: 'ready'
+    },
+    {
+        status: 'ready'
+    },
+    {
+        status: 'ready'
+    },
+    {
+        status: 'ready'
+    }
+]
+
+const pomodoroReducer = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state
+    }
+}
+
+export default pomodoroReducer
